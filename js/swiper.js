66fcdb8a44d4c25d253ecf4730732ca06bdb6350
Victoria Serzhanova <victoriaserzhanova@gmail.com>
2021-08-08 12:15:10 +0300

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper-container',  {
+	loop: true,
+	loopedSlides: 3,
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	spaceBetween: 33,
+	pagination: {
+		el: '.hero-pagination',
+		clickable: true,
+	},
+});
+
+console.log('swiper');
+
+
+
